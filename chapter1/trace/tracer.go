18ffb6a17e1cc5e166d56751fdb2cfd1b4bd7040
pomadev <y.pomadev@gmail.com>
2019-06-05 13:48:04 +0900

    tracer.goの実装改善

--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -5,29 +5,18 @@ import (
 	"io"
 )
 
-// Tracerはコード内での出来事を記録できるオブジェクトを表すインターフェースです。
-type Tracer interface {
-	Trace(...interface{})
-}
-
-func New(w io.Writer) Tracer {
-	return &tracer{out: w}
-}
-
-type tracer struct {
+// Tracerはコード内での出来事を記録します。
+type Tracer struct {
 	out io.Writer
 }
 
-func (t *tracer) Trace(a ...interface{}) {
-	t.out.Write([]byte(fmt.Sprint(a...)))
-	t.out.Write([]byte("\n"))
+func (t Tracer) Trace(a ...interface{}) {
+	if t.out == nil {
+		return
+	}
+	fmt.Fprintln(t.out, a...)
 }
 
-type nilTracer struct{}
-
-func (t *nilTracer) Trace(a ...interface{}) {}
-
-// OffはTraceメソッドの呼び出しを無視するTracerを返します。
-func Off() Tracer {
-	return &nilTracer{}
+func New(w io.Writer) Tracer {
+	return Tracer{out: w}
 }
