08f06fcb66496de8af96695c4e68497c48c1e967
pomadev <y.pomadev@gmail.com>
2019-06-06 01:07:03 +0900

    chapter2実装完了

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"flag"
+	"github.com/go-ini/ini"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+	"github.com/stretchr/signature"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,15 +29,35 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.Must(template.ParseFiles(filepath.Join("templates",
 				t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	// Gomniauthのセットアップ
+	cfg, err := ini.Load("chat.ini")
+	if err != nil {
+		log.Fatal("Fail to read file: ", err)
+	}
+	gomniauth.SetSecurityKey(signature.RandomKey(64))
+	gomniauth.WithProviders(
+		facebook.New(cfg.Section("auth").Key("facebook_client_key").String(), cfg.Section("auth").Key("facebook_secret_key").String(), "http://localhost:8080/auth/callback/facebook"),
+		github.New(cfg.Section("auth").Key("github_client_key").String(), cfg.Section("auth").Key("github_secret_key").String(), "http://localhost:8080/auth/callback/github"),
+		google.New(cfg.Section("auth").Key("google_client_key").String(), cfg.Section("auth").Key("google_secret_key").String(), "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
